b1f26480f20360fdc633aa6d2f3116b145988790
majerhua <majerhua123@gmail.com>
2022-10-21 09:57:24 -0500

    style

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -7,11 +7,27 @@ export const RegisterSchema = yup.object().shape({
   .matches(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, 'El valor ingresado no es un correo'),
   identity_document: yup.string()
   .required('Completar este campo')
-  .matches(/^[0-9]+$/, 'Solo se aceptan dígitos'),
+  .when('type_document', (type_document) => {
+    if (type_document === '1') {
+        return yup.string()
+        .required('Completar este campo')
+        .matches(/^[0-9]+$/, 'Solo se aceptan dígitos')
+        .matches(/^[1-7].*$/, 'Debe comenzar con un numero del 1 al 7')
+        .length(9, 'Deben haber 9 dígitos')
+    } else if(type_document === '2'){
+      return yup.string()
+      .required('Completar este campo')
+      .matches(/^[0-9]+$/, 'Solo se aceptan dígitos')
+      .matches(/^[8-9].*$/, 'Debe comenzar con un numero del 8 al 9')
+      .length(12, 'Deben haber 12 dígitos') 
+    }
+  }),
   type_document: yup.string().required('Completar este campo'),
   phone: yup.string()
   .required('Completar este campo')
-  .matches(/^[0-9]+$/, 'Solo se aceptan dígitos'),
+  .matches(/^[0-9]+$/, 'Solo se aceptan dígitos')
+  .min(8, 'No puede tener menos de 8 dígitos')
+  .max(9, 'No puede tener más de 9 dígitos'),
   is_suscriptor: yup.string().required('Completar este campo'),
   terms_conditions: yup.string().nullable().required('Completar este campo')
 })
